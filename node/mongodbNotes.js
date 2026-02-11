52d6690acefb861e87e8960ae5099cd23c6174a0
shubham kumar <shubhamfarainzi@gmail.com>
2026-02-11 20:57:59 +0530

    updated

--- node/mongodbNotes.js
+++ node/mongodbNotes.js
@@ -1,5 +1,5 @@
 // Mongodb  --> it is a no sql database where data is stored in JSON format
-// 
+// downlaod mongodb -->https://www.mongodb.com/try/download/community
 
 // mongodb commands --> CRUD
 
@@ -12,7 +12,7 @@
 //4) insert data inside collection --> 
     // a) insert single Data --> db.collectionName.insertOne({key:value})
         // example --> db.users.insertOne({name:"one",email:"one@gmail.com"})
-        
+
     // b) multiple data --> db.collectionName.insertMany([{key:value}, {key:value}])
 
     // example --> db.products.insertMany([
@@ -20,6 +20,7 @@
     //     {name:"MI", price:45000, rating:5},
     //     {name:"samsung", price:45000, rating:5},
     // ])
+
 
 
     let arr = [
