14ab15279bac33b483bfc9ccc946a3b847c5e92a
shubham kumar <shubhamfarainzi@gmail.com>
2026-02-17 19:06:09 +0530

    added new github project

---
+++ git&github/githubNotes.js
@@ -0,0 +1,28 @@
+// Git-->git is a tool that helps you save ,track and manage the changes in your code and file
+
+
+// git  is like a time machine for your project. it remembers every change yo make, so you can go back to old version, fix mistake, and work with others.
+
+// it is a verion control system
+
+// Commands --> 
+//1) create a folder --> mkdir folderName
+//2) create a file --> touch fileName or echo>fileName
+
+//3) git init --> create a empt git repository (folder)
+//4)add a file in stagging area --> git add fileName
+//5)add all files and folder in stagin area --> git add .
+//6) git status -->shows your which files are in stagged area , which file are to be commited ,which file are untracked
+
+//git commit -m "message"  --> take a snapshot of your project and save the history of that file in .git repository
+
+// show all commit history --> git log
+
+// upload project online on github using below commands --> 
+// 1) create a account on github
+
+// 2)git config --global user.name "github userName"
+// 3)git config --global user.email "github email"
+
+// 4)git remote add origin https://github.com/shubhamkumar123456/G4github.git  --> add your local system .git repository to online github
+// 5)git push origin main --> push all your local system code to online connected repository
