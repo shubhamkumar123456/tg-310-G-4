3850a9cf2467bf320af1b45c28f8e090f090c381
shubham kumar <shubhamfarainzi@gmail.com>
2026-02-18 20:45:59 +0530

    updated github notes

--- git&github/githubNotes.js
+++ git&github/githubNotes.js
@@ -9,20 +9,30 @@
 //1) create a folder --> mkdir folderName
 //2) create a file --> touch fileName or echo>fileName
 
-//3) git init --> create a empt git repository (folder)
+//3) git init --> create a empty git repository (folder)
 //4)add a file in stagging area --> git add fileName
 //5)add all files and folder in stagin area --> git add .
 //6) git status -->shows your which files are in stagged area , which file are to be commited ,which file are untracked
 
-//git commit -m "message"  --> take a snapshot of your project and save the history of that file in .git repository
+//7)git commit -m "message"  --> take a snapshot of your project and save the history of that file in .git repository
 
-// show all commit history --> git log
+//8) show all commit history --> git log
 
 // upload project online on github using below commands --> 
 // 1) create a account on github
-
 // 2)git config --global user.name "github userName"
 // 3)git config --global user.email "github email"
+// 4) rename your current branch -->git branch -M newName
+// 5)git remote add origin https://github.com/shubhamkumar123456/G4github.git  --> add your local system .git repository to online github
+// 6)git push origin main --> push all your local system code to online connected repository
+//7) show all branches --> git branch
+//8) create a new branch  --> git branch Branchname
+//9) switch a branch --> git checkout branchName
+//10) copy code from anothers repository to your github repo --> do fork
+//11) copy all your gihub repo code to local machine --> git clone "github repo url"
+// 12)connect your repo to original repository from where you have forked project so you can get latest updates --> git remote add upstream "github url from you froked code"
+// 
+//13) git clone  --> copy all your github repo link to your local machine
+//14) git pull  --> fetch and install updates coming from github repo
 
-// 4)git remote add origin https://github.com/shubhamkumar123456/G4github.git  --> add your local system .git repository to online github
-// 5)git push origin main --> push all your local system code to online connected repository
+
