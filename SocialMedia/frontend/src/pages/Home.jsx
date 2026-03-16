df5e4ae9ec2dec1fda1f52d5ab3269449aae6883
shubham kumar <shubhamfarainzi@gmail.com>
2026-03-16 20:57:04 +0530

    added post Ui in home

--- SocialMedia/frontend/src/pages/Home.jsx
+++ SocialMedia/frontend/src/pages/Home.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { RiVideoUploadLine } from "react-icons/ri";
+import EmojiPicker from 'emoji-picker-react';
+import { BsEmojiSmile } from "react-icons/bs";
 
 const Home = () => {
+
+  let titleRef = useRef()  //{current:undefined}
+  const [x, setX] = useState(false);
+  console.log(x)
+
+  let token = localStorage.getItem('G4Auth');
+  console.log(token)
+
+
+  function handleEmojiClicker(e){
+    // console.log(e)
+    console.log(e.emoji)
+    let inputvalue = titleRef.current.value;
+    console.log(inputvalue)
+
+    let ans = inputvalue + e.emoji;
+    console.log(ans)
+    titleRef.current.value = ans
+  }
+
   return (
-    <div className='bg-red-400'>
+    <div className=''>
         
       <h1>This is home page</h1>
+
+      <div className='border rounded flex gap-4 flex-col p-8 w-[50%] mx-auto'>
+        <textarea ref={titleRef} className='border rounded p-3' name="" id="" placeholder='whats on your mind..?'></textarea>
+        <input hidden id='a' type="file" />
+
+      <div className='flex items-center gap-9'>
+
+         <label htmlFor="a">
+            <RiVideoUploadLine size={30} color='blue'/>
+        </label>
+
+      <BsEmojiSmile onClick={()=>setX(!x)} size={25} color='green'/>
+      </div>
+
+      <EmojiPicker onEmojiClick={handleEmojiClicker} theme='dark' autoFocusSearch='false' open={x}/>
+
+        
+        <button>Post</button>
+      </div>
     </div>
   )
 }
--- SocialMedia/frontend/src/pages/Login.jsx
+++ SocialMedia/frontend/src/pages/Login.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useNavigate } from 'react-router-dom';
+import {  toast } from 'react-toastify';
 
 const Login = () => {
+
+   
+    let emailRef = useRef()
+    let passwordRef = useRef()
+
+    let navigate = useNavigate()
+
+
+    async function handleSubmit(e){
+        e.preventDefault()
+        console.log("hello")
+        let obj ={
+            email:emailRef.current.value,
+            password:passwordRef.current.value
+        }
+        // console.log(obj)
+        let res = await fetch('http://localhost:8090/users/login',{
+            method:"POST",
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(obj)
+        })
+
+        let data = await res.json();
+        console.log(data)
+        if(res.status==200 || res.status==201){
+            toast.success(data.msg)
+            localStorage.setItem('G4Auth',JSON.stringify(data.token))
+            navigate('/')
+        }
+        else{
+            toast.error(data.msg)
+        }
+        
+    }
+
   return (
     <div>
-      this is login page
+        <form action="" className='flex flex-col gap-3 bg-black p-8 text-white w-[50%] mx-auto'>
+          
+            <label htmlFor="">Email</label>
+            <input ref={emailRef} className='border p-2' type="text" placeholder='enter your email' />
+            <label htmlFor="">Password</label>
+            <input ref={passwordRef} className='border p-2' type="text" placeholder='enter your password' />
+            <button onClick={handleSubmit} className='bg-green-700'>Submit</button>
+
+        </form>
     </div>
   )
 }
