91a6d1ddc211db626cd1d09e004dddf78e13b469
shubham kumar <shubhamfarainzi@gmail.com>
2026-04-27 20:15:46 +0530

    updated db file

--- SocialMedia/backend/config/db.js
+++ SocialMedia/backend/config/db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 async function connection(){
    try {
-    await mongoose.connect('mongodb://localhost:27017/projectG4')
+    await mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@socialmediag4.gxyjfou.mongodb.net/?appName=SocialmediaG4`)
    console.log("mongodb connected")
    } catch (error) {
 console.log("error in connecting mongodb")
